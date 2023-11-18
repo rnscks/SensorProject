3ed1209fc46887fd88be5e58cd84cc700612fc02
rnscks <mplngx2@gmail.com>
2023-11-18 22:38:57 +0900

    Celsius Range Modified

--- DataFilter.py
+++ DataFilter.py
@@ -6,8 +6,6 @@ class DataFilter:
         
     def Filtering(self) -> None:    
         self.ExResultDataSet = self.ExResultDataSet[self.ExResultDataSet['Celsius'] < 30]
-        self.ExResultDataSet = self.ExResultDataSet[20 <= self.ExResultDataSet['Celsius']]
-        
         self.ExResultDataSet['Set Number'] = range(len(self.ExResultDataSet))
         
         return self.ExResultDataSet
